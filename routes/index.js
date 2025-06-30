1638135e6cd80e0f8162ab3c31068cc7b36af328
Dominick Goehrig <dgdomgoe3@gmail.com>
2025-06-30 00:05:21 -0400

    "third"

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const itemRouter = require("./clothingItem");
-const { NOt_FOUND } = require("../utils/errors");
+const { NOT_FOUND } = require("../utils/errors");
 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
 router.use((req, res) =>
-  res.status(NOt_FOUND).send({ message: "Requested resource not found" })
+  res.status(NOT_FOUND).send({ message: "Requested resource not found" })
 );
 
 module.exports = router;
