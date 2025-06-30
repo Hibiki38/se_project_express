b1664a78a73349f20e838afc0288d65960915c0e
Dominick Goehrig <dgdomgoe3@gmail.com>
2025-06-30 00:03:32 -0400

    "second iteration"

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const itemRouter = require("./clothingItem");
-const { Not_Found } = require("../utils/errors");
+const { NOt_FOUND } = require("../utils/errors");
 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
 router.use((req, res) =>
-  res.status(Not_Found).send({ message: "Requested resource not found" })
+  res.status(NOt_FOUND).send({ message: "Requested resource not found" })
 );
 
 module.exports = router;
